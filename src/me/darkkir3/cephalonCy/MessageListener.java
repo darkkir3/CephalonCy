aa3ca2f9db796ed2e7d49af1b89f161f5872718d
Dstrecker <Blackboard01!>
2019-08-20 22:23:10 +0200

    removed comments

--- src/me/darkkir3/cephalonCy/MessageListener.java
+++ src/me/darkkir3/cephalonCy/MessageListener.java
@@ -28,17 +28,5 @@ public class MessageListener extends ListenerAdapter
     			c.sendMessage(event.getMessage()).addFile(new File("test.txt"), AttachmentOption.SPOILER);
     		});
     	}
-    	
-        /*if (event.isFromType(ChannelType.PRIVATE))
-        {
-            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
-                                    event.getMessage().getContentDisplay());
-        }
-        else
-        {
-            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
-                        event.getTextChannel().getName(), event.getMember().getEffectiveName(),
-                        event.getMessage().getContentDisplay());
-        }*/
     }
 }
